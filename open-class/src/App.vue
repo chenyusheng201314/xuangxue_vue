c488e1fecd7640e8201c7c890c636ab19968cef8
chenyusheng201314 <1835788675@qq.com>
2018-07-25 13:36:32 +0800

    cys
    公开课

---
+++ open-class/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  #app {
+    width: 1200px;
+    height: auto;
+    overflow: hidden;
+    margin: 0 auto;
+  }
+  ul,li,ol{
+    list-style-type: none;
+  }
+</style>
